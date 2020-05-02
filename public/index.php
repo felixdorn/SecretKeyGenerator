057c16d9f1e80efd31c1d812c5828cceaf1ca1d9
Félix Dorn <github@felixdorn.fr>
2020-05-02 20:13:17 +0200

    :truck: rewrite in siler

---
+++ public/index.php
@@ -0,0 +1,29 @@
+<?php
+
+use Kuria\Error\ErrorHandler;
+use function Siler\Dotenv\env;
+use function Siler\Route\get;
+use function Siler\Twig\init;
+use function Siler\Twig\render;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$env = $_ENV['APP_ENV'] ?? 'production';
+$handler = (new ErrorHandler());
+$handler->setDebug(true);
+$handler->register();
+
+init(dirname(__DIR__) . '/views');
+
+get('/', function () {
+    $length = $_GET['length'] ?? env('KEY_DEFAULT_LENGTH', 64);
+    echo render('index.twig', [
+        'key' => generateKey($length),
+         'length'  => $length
+    ]);
+});
+
+get('/text', function () {
+    $length = $_GET['length'] ?? env('KEY_DEFAULT_LENGTH', 64);
+    echo  generateKey($length);
+});
---
+++ src/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+use function Siler\Dotenv\env;
+
+function stop(string $error, ...$context): void {
+    echo sprintf($error, $context);
+
+    if (function_exists('fastcgi_finish_request')) {
+        fastcgi_finish_request();
+    }
+    exit();
+}
+
+function generateKey(int $length) {
+    $max = env('KEY_MAX_LENGTH', 1024);
+
+    if ($length <= 0) {
+        stop('Length should be greater than 0');
+    }
+
+    if ($length > $max) {
+        stop('Length should be greater than %s', $max);
+    }
+
+    return substr(base64_encode(
+        random_bytes($length)
+    ), $length);
+}
